74ec5a2291c969a12f1f3029543397a919489a60
MerlinMDV <64904743+MerlinMDV@users.noreply.github.com>
2024-07-02 16:59:59 -0700

    finished basic page

--- js/main.js
+++ js/main.js
@@ -1,23 +1,26 @@
-let xp = 0;
-let percent = 0;
-let goalxp = 0;
-let lvl = parseInt(document.getElementById("lvl").value);
-let goal = parseInt(document.getElementById("goal").value);
-let output = document.getElementById('output');
+function mathMain() {
+  let xp = 0;
+  let percent = 0;
+  let goalxp = 0;
+  let lvl = parseInt(document.getElementById("lvl").value);
+  let goal = parseInt(document.getElementById("goal").value);
 
-for (let i = 0; i < goal; i++) {
-  goalxp += ((200 * i) - 100);
-}
-
-for (let i = 0; i < lvl; i++) {
-  if (lvl < 2) {
-    xp = 0;
-    break;
+  for (let i = 0; i < goal; i++) {
+    goalxp += ((200 * i) - 100);
   }
-  xp += ((200 * i) - 100);
+
+  for (let i = 0; i < lvl; i++) {
+    if (lvl < 2) {
+      xp = 0;
+      break;
+    }
+    xp += ((200 * i) - 100);
+  }
+
+  percent = (xp / goalxp);
+
+  document.getElementById("output").value = `You have gained a total of ~${xp} xp.\nYou are ${Math.round((percent*100), 2)}% of the way to level ${goal}.`
+
+  console.log(`You have gained a total of ~${xp} xp.`);
+  console.log(`You are ${Math.round((percent*100), 2)}% of the way to level ${goal}.`);
 }
-
-percent = (xp / goalxp);
-
-console.log(`You have gained a total of ~${xp} xp.`);
-console.log(`You are ${Math.round((percent*100), 2)}% of the way to level ${goal}.`);
